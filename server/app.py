c96af78ae98227aff58c97e2393fa3606c7ba55b
Rakesh Ravi Shankar <Rakesh@dyn-129-236-229-201.dyn.columbia.edu>
2017-10-15 04:24:20 -0400

    Fixed server queries

--- server/app.py
+++ server/app.py
@@ -1,8 +1,12 @@
 #!flask/bin/python
 from flask import Flask, jsonify, g, request
 import sqlite3
+from User import User
 
 app = Flask(__name__)
+
+current_user = User()
+
 
 def get_db():
 	db = getattr(g, '_database', None)
@@ -39,10 +43,25 @@ def insert(db, table, fields=(), values=()):
 
 @app.route('/login', methods=['GET'])
 def login():
+	global current_user
 	db = get_db()
 	google_id = request.args.get('google_id', '')
 	user = query_db(db, 'select * from Users where GoogleId=' + google_id, (), True)
+	current_user.name = user.name
+	current_user.phone = user.phone
+	current_user.google_id = user.google_id
+	current_user.governing_locations = user.governing_locations.split(',')
 	return jsonify(user)
+
+@app.route('/fetch-governed-locations', methods=['GET'])
+def fetch_governed_locations():
+	db = get_db()
+	governed_locations = []
+	for governing_location in current_user.governing_locations:
+		query_result = query_db(db, 'select * from GovernedLocations where id=' + governing_location, (), True)
+		governed_locations.append(query_result)
+	print governed_locations
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
