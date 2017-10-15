1ec2452add29d9f0d1830a5ed358e4660d9133ad
Amritansh Tripathi <atripathi@chewy.com>
2017-10-15 02:21:28 -0400

    added unversiioned files.

---
+++ android/Govern2/app/src/main/java/govern/ny/hack/edu/govern/UserModel.java
@@ -0,0 +1,42 @@
+package govern.ny.hack.edu.govern;
+
+
+public class UserModel {
+
+    private String mUserName;
+    private String mPhotourl;
+    private String mPhoneNumber;
+    private String mUid;
+
+    public String getmUserName() {
+        return mUserName;
+    }
+
+    public void setmUserName(String mUserName) {
+        this.mUserName = mUserName;
+    }
+
+    public String getmPhotourl() {
+        return mPhotourl;
+    }
+
+    public void setmPhotourl(String mPhotourl) {
+        this.mPhotourl = mPhotourl;
+    }
+
+    public String getmPhoneNumber() {
+        return mPhoneNumber;
+    }
+
+    public void setmPhoneNumber(String mPhoneNumber) {
+        this.mPhoneNumber = mPhoneNumber;
+    }
+
+    public String getmUid() {
+        return mUid;
+    }
+
+    public void setmUid(String mUid) {
+        this.mUid = mUid;
+    }
+}
